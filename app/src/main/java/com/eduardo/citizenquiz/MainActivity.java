5befd7f5b2657df237ad958503c7753d5857207b
eduardo <ecsunahata@indracompany.com>
2019-08-01 17:07:17 -0500

    send value from main to second

--- app/src/main/java/com/eduardo/citizenquiz/MainActivity.java
+++ app/src/main/java/com/eduardo/citizenquiz/MainActivity.java
@@ -2,13 +2,36 @@ package com.eduardo.citizenquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    Button btnCall2nd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCall2nd = findViewById(R.id.btn_call_2nd);
+        btnCall2nd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "onClick: btnCall2nd");
+                startSecondActivity();
+            }
+        });
+    }
+
+    private void startSecondActivity(){
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra("hidden_id","0001");
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/eduardo/citizenquiz/SecondActivity.java
+++ app/src/main/java/com/eduardo/citizenquiz/SecondActivity.java
@@ -3,12 +3,20 @@ package com.eduardo.citizenquiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+
+    TextView tvMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        String valueFromMainActivity = getIntent().getStringExtra("hidden_id");
+
+        tvMessage = findViewById(R.id.tv_message);
+        tvMessage.setText(valueFromMainActivity);
     }
 }
